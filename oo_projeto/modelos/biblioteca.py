0c94edf55013365606398ef5907bcc2de8ab11c9
Matheus-Nobre <matheusipsnobre@gmail.com>
2026-02-01 12:55:10 -0300

    modularizando

--- oo_projeto/modelos/biblioteca.py
+++ oo_projeto/modelos/biblioteca.py
@@ -1,29 +1,23 @@
 class Biblioteca:
-  bibliotecas = []
-  
-  def __init__(self, name):
-    self.name = name
-    self._ativo = False
-    Biblioteca.bibliotecas.append(self)
-    
-  def __str__(self):
-    return self.name
-  
-  def listar_bibliotecas():
-    for biblioteca in Biblioteca.bibliotecas:
-      print(f"{biblioteca}")
-      
-  def alterna_estado(self):
-    self._ativo = not self._ativo
-  
-  @property
-  def ativo(self):
-   return "ativado" if self._ativo else "desativado"
-   
-  
+    bibliotecas = []
 
-biblioteca_cidade = Biblioteca("Biblioteca da Cidade")
-biblioteca_shopping = Biblioteca("Biblioteca do Shopping")
+    def __init__(self, name):
+        self.name = name
+        self._ativo = False
+        Biblioteca.bibliotecas.append(self)
 
-Biblioteca.listar_bibliotecas()
-print(vars(biblioteca_cidade))
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def listar_bibliotecas(cls):
+        print(f"{'Nome da biblioteca:'.ljust(20)} | {'Status:'}")
+        for biblioteca in Biblioteca.bibliotecas:
+            print(f"{biblioteca} | {biblioteca.ativo}")
+
+    def alterna_estado(self):
+        self._ativo = not self._ativo
+
+    @property
+    def ativo(self):
+        return "ativado" if self._ativo else "desativado"
