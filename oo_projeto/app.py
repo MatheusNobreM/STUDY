8428b0a1ad77e1617e5bcc2cdb0c4a109a92f67b
Matheus-Nobre <matheusipsnobre@gmail.com>
2026-02-02 18:15:28 -0300

    adicionando livro e revista

--- oo_projeto/app.py
+++ oo_projeto/app.py
@@ -1,10 +1,15 @@
 from modelos.biblioteca import Biblioteca
+from modelos.itens.livro import Livro
+from modelos.itens.revista import Revista
 
 biblioteca_cidade = Biblioteca("Biblioteca da Cidade")
 biblioteca_shopping = Biblioteca("Biblioteca do Shopping")
 
-biblioteca_shopping.receber_avaliacao("Fulano", 9)
-biblioteca_shopping.receber_avaliacao("2", 10)
+# biblioteca_shopping.receber_avaliacao("Fulano", 9)
+# biblioteca_shopping.receber_avaliacao("2", 10)
+
+livro1 = Livro("1984", "George Orwell", 30.0, "0800-987")
+revista1 = Revista("National Gergraphic", "Jorge", 20.0, "Quinta")
 
 
 def main():
