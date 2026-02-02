ac26c6233717e96fa744fbc7d0c9b43986af4254
Matheus-Nobre <matheusipsnobre@gmail.com>
2026-02-02 19:48:34 -0300

    projeto de poo completo

--- oo_projeto/app.py
+++ oo_projeto/app.py
@@ -12,8 +12,15 @@ livro1 = Livro("1984", "George Orwell", 30.0, "0800-987")
 revista1 = Revista("National Gergraphic", "Jorge", 20.0, "Quinta")
 
 
+biblioteca_cidade.adicionar_item(livro1)
+biblioteca_cidade.adicionar_item(revista1)
+
+livro1.aplicar_desconto()
+
+
 def main():
-    Biblioteca.listar_bibliotecas()
+    # Biblioteca.listar_bibliotecas()
+    biblioteca_cidade.exibir_itens()
 
 
 if __name__ == "__main__":
--- oo_projeto/modelos/biblioteca.py
+++ oo_projeto/modelos/biblioteca.py
@@ -1,4 +1,5 @@
 from modelos.avaliacao import Avaliacao
+from modelos.itens.item_biblioteca import ItemBiblioteca
 
 
 class Biblioteca:
@@ -8,6 +9,7 @@ class Biblioteca:
         self.name = name
         self._ativo = False
         self._avaliacao = []
+        self._itens = []
         Biblioteca.bibliotecas.append(self)
 
     def __str__(self):
@@ -42,3 +44,17 @@ class Biblioteca:
         media = round(soma / len(self._avaliacao), 1)
 
         return media
+
+    def adicionar_item(self, item):
+        if isinstance(item, ItemBiblioteca):
+            self._itens.append(item)
+
+    def exibir_itens(self):
+        print(f"Itens da Biblioteca {self.name}\n")
+        for i, item in enumerate(self._itens, start=1):
+            if hasattr(item, "isbn"):
+                msg_livro = f"{i}. (Livro) Título: {item._titulo} | Autor: {item._autor} | Preço: {item._preco} | ISBN: {item.isbn}"
+                print(msg_livro)
+            else:
+                msg_revista = f"{i}. (Revista) Título: {item._titulo} | Autor: {item._autor} | Preço: {item._preco} | Edição: {item.edicao}"
+                print(msg_revista)
