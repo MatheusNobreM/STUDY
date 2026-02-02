a1c23bcd3777cef452f4a22b66a935b5087e8082
Matheus-Nobre <matheusipsnobre@gmail.com>
2026-02-02 17:50:24 -0300

    adicionado o modulo de avaliação

--- oo_projeto/app.py
+++ oo_projeto/app.py
@@ -1,7 +1,10 @@
 from modelos.biblioteca import Biblioteca
 
-biblioteca_shopping = Biblioteca("Biblioteca da Cidade")
+biblioteca_cidade = Biblioteca("Biblioteca da Cidade")
 biblioteca_shopping = Biblioteca("Biblioteca do Shopping")
+
+biblioteca_shopping.receber_avaliacao("Fulano", 9)
+biblioteca_shopping.receber_avaliacao("2", 10)
 
 
 def main():
--- oo_projeto/modelos/biblioteca.py
+++ oo_projeto/modelos/biblioteca.py
@@ -1,9 +1,13 @@
+from modelos.avaliacao import Avaliacao
+
+
 class Biblioteca:
     bibliotecas = []
 
     def __init__(self, name):
         self.name = name
         self._ativo = False
+        self._avaliacao = []
         Biblioteca.bibliotecas.append(self)
 
     def __str__(self):
@@ -11,9 +15,13 @@ class Biblioteca:
 
     @classmethod
     def listar_bibliotecas(cls):
-        print(f"{'Nome da biblioteca:'.ljust(20)} | {'Status:'}")
+        print(
+            f"{'Nome da biblioteca:'.ljust(25)} | {'Nota média'.ljust(25)} | {'Status:'}"
+        )
         for biblioteca in Biblioteca.bibliotecas:
-            print(f"{biblioteca} | {biblioteca.ativo}")
+            print(
+                f"{biblioteca.name.ljust(25)} | {str(biblioteca.media_avaliacao).ljust(25)} | {biblioteca.ativo}"
+            )
 
     def alterna_estado(self):
         self._ativo = not self._ativo
@@ -21,3 +29,16 @@ class Biblioteca:
     @property
     def ativo(self):
         return "ativado" if self._ativo else "desativado"
+
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacao(self):
+        if not self._avaliacao:
+            return "-"
+        soma = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        media = round(soma / len(self._avaliacao), 1)
+
+        return media
