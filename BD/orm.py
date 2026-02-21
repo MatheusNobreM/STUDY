ea99b47bdcce0eef471ff3c99f94d77f6ae88963
Matheus-Nobre <matheusipsnobre@gmail.com>
2026-02-21 16:43:11 -0300

    orm

---
+++ BD/orm.py
@@ -0,0 +1,48 @@
+from sqlalchemy import Column, Float, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("sqlite:///banco.db", echo=True)
+Base = declarative_base()
+
+
+class Filme(Base):
+    __tablename__ = "filmes"
+    id = Column(Integer, primary_key=True)
+    nome = Column(String, nullable=False)
+    ano = Column(Integer, nullable=False)
+    nota = Column(Float, nullable=False)
+
+
+Base.metadata.create_all(engine)
+
+
+def adiciona_filme(nome, ano, nota):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    filme = Filme(nome=nome, ano=ano, nota=nota)
+    session.add(filme)
+    session.commit()
+    session.close()
+
+
+# adiciona_filme("Mario", 2024, 8)
+# adiciona_filme("Sonic", 2020, 6.5)
+
+
+def atualiza_filme(id, nome=None, ano=None, nota=None):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    filme = session.query(Filme).filter_by(id=id).first()
+    if filme:
+        if nome is not None:
+            filme.nome = nome
+        if ano is not None:
+            filme.ano = ano
+        if nota is not None:
+            filme.nota = nota
+        session.commit()
+    session.close()
+
+
+atualiza_filme(1, "Homem de ferro", 2013, 9)
