cb57055bf89c8f707e2551d075945cc04ad53500
Matheus-Nobre <matheusipsnobre@gmail.com>
2026-03-12 22:05:51 -0300

    api completa

--- FastAPI/farm1/routes/jogador.py
+++ FastAPI/farm1/routes/jogador.py
@@ -1,13 +1,47 @@
 from fastapi import APIRouter
 from config.database import conexao
 from models.jogador import Jogador
+from schemas.jogador import jogadorEntidade, listaJogadoresEntidade
+from bson import ObjectId
 
 jogador_router = APIRouter()
 
+
 @jogador_router.get("/")
 async def inicio():
-  return "Bem vindo ao FullStack Farm"
+    return "Bem vindo ao FullStack Farm"
+
 
 @jogador_router.get("/jogadores")
 async def lista_jogadores():
-  return conexao.local.jogador.find()
+    return listaJogadoresEntidade(conexao.local.jogador.find())
+
+
+@jogador_router.get("/jogadores/{jogador_id}")
+async def busca_jogador_id(jogador_id):
+    return jogadorEntidade(
+        conexao.local.jogador.find_one({"_id": ObjectId(jogador_id)})
+    )
+
+
+@jogador_router.post("/jogadores")
+async def cadastra_jogadores(jogador: Jogador):
+    conexao.local.jogador.insert_one(dict(jogador))
+    return listaJogadoresEntidade(conexao.local.jogador.find())
+
+
+@jogador_router.put("/jogadores/{jogadores_id}")
+async def atualiza_jogador(jogador_id, jogador: Jogador):
+    conexao.local.jogador.find_one_and_update(
+        {"_id": ObjectId(jogador_id)}, {"$set": dict(jogador)}
+    )
+    return jogadorEntidade(
+        conexao.local.jogador.find_one({"_id": ObjectId(jogador_id)})
+    )
+
+
+@jogador_router.delete("/jogadores/{jogador_id}")
+async def exclui_jogador(jogador_id):
+    return jogadorEntidade(
+        conexao.local.jogador.find_one({"_id": ObjectId(jogador_id)})
+    )
