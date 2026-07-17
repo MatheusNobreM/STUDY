24334b0b6a843f10f476e6e0803fe743f77a18a9
Matheus Moreira <matheusipsnobre@gmail.com>
2026-07-17 11:58:59 -0300

    exemplo

---
+++ Metodos Abstratos/ex1.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14159 * self.radius**2
+
+    def perimeter(self):
+        return 2 * 3.14159 * self.radius
+
+
+# Uncommenting the following line will raise an error because we’re missing method implementations
+# shape = Shape() # TypeError: Can't instantiate abstract class Shape with abstract methods #area, perimeter
+
+circle = Circle(5)
+print(f"Area: {circle.area()}")
+print(f"Perimeter: {circle.perimeter()}")
