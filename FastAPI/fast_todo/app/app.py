8c5e6e236f376f312b4001f8c00ad1d6decde619
Matheus Moreira <matheusipsnobre@gmail.com>
2026-07-24 15:03:50 -0300

    config inicial

--- FastAPI/fast_todo/app/app.py
+++ FastAPI/fast_todo/app/app.py
@@ -1,8 +1,28 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
+from beanie import init_beanie
 from fastapi import FastAPI
+from pymongo import AsyncMongoClient
 
-app = FastAPI()
+from core.config import settings
 
 
-@app.get("/")
-async def hello():
-    return "Hello Word"
+@asynccontextmanager
+async def lifespan(_: FastAPI) -> AsyncGenerator[None, None]:
+    client = AsyncMongoClient(settings.MONGO_CONNECTION_STRING)
+    database = client.todoapp
+
+    await init_beanie(database=database, document_models=[])
+
+    try:
+        yield
+    finally:
+        await client.close()
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    lifespan=lifespan,
+)
